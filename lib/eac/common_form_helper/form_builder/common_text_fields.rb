dee927d7e2d8eee40a9428b8dc163590d6d29168
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-06-23 17:16:42 -0300

    Suporte a opções par o input de campos de texto de common_form. Refs #546.

--- lib/eac/common_form_helper/form_builder/common_text_fields.rb
+++ lib/eac/common_form_helper/form_builder/common_text_fields.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Eac
   module CommonFormHelper
     class FormBuilder
@@ -6,8 +7,10 @@ module Eac
           class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
             def #{t}_field(field_name, options = {})  # def text_field(field_name, options = {})
               field(field_name, options) do           #   field(field_name, options) do
+                input_options = options[:input_options] || {}
+                input_options[:class] ||= 'form-control'
                 @form.#{t}_field(field_name,          #     @form.text_field(field_name,
-                  class: 'form-control')              #       class: 'form-control')
+                  input_options)              #       class: 'form-control')
               end                                     #   end
             end                                       # end
             RUBY_EVAL
