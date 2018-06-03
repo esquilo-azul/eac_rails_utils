5088c091885a6c89b374f04a35a8140793204e0f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-06-02 23:12:13 -0300

    Autocoerce time-array values in EacRailsUtils::TablelessModel.

---
+++ test/lib/eac_rails_utils/tableless_model_test.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+require 'test_helper'
+
+module EacRailsUtils
+  class TablelessModelTest < ActiveSupport::TestCase
+    class Stub < ::EacRailsUtils::TablelessModel
+      attribute :tempo, DateTime
+    end
+
+    test 'date time array values' do
+      stub = Stub.new('tempo(1i)' => '9', 'tempo(2i)' => '10', 'tempo(3i)' => '11',
+                      'tempo(4i)' => '12', 'tempo(5i)' => '13', 'tempo(6i)' => '14')
+      assert stub.tempo.is_a?(DateTime), "Class: |#{stub.tempo.class}|, Value: |#{stub.tempo}|"
+      assert_equal 9, stub.tempo.year, 'Year'
+      assert_equal 10, stub.tempo.month, 'Month'
+      assert_equal 11, stub.tempo.day, 'Day'
+      assert_equal 12, stub.tempo.hour, 'Hour'
+      assert_equal 13, stub.tempo.minute, 'Minute'
+      assert_equal 14, stub.tempo.second, 'Second'
+    end
+  end
+end
