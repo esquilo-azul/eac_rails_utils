a10a0e67491976198dcbcb26b7c11a773a804a7b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-12-07 22:41:15 -0300

    EacRailsUtils::LinksHelper.object_path: search path in object class' superclasses.

--- app/helpers/eac_rails_utils/links_helper.rb
+++ app/helpers/eac_rails_utils/links_helper.rb
@@ -36,9 +36,16 @@ module EacRailsUtils
     end
 
     def object_path(object, action = nil)
-      path = "#{object.class.name.underscore.tr('/', '_')}_url"
-      path = "#{action}_#{path}" if action.present?
-      send(path, object)
+      current_class = object.class
+      tried_paths = []
+      while current_class
+        path = object_path_by_class(current_class, action)
+        return send(path, object) if respond_to?(path)
+        tried_paths << path
+        current_class = current_class.superclass
+      end
+      raise "Path not found for {object: #{object.class}, action: \"#{action}\"}" \
+        "(Tried: #{tried_paths})"
     end
 
     private
@@ -49,6 +56,12 @@ module EacRailsUtils
                 class: 'show_link', target: '_blank',
                 title: ::I18n.t('eac_rails_base0.links.show_object', label: value.to_s)
       end
+    end
+
+    def object_path_by_class(klass, action)
+      path = "#{klass.name.underscore.tr('/', '_')}_url"
+      path = "#{action}_#{path}" if action.present?
+      path
     end
   end
 end
