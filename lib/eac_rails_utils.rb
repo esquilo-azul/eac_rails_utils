3e3f4d7075ee7e0bfff01c115c629b9e177e3558
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-02-11 19:15:32 -0300

    Eac::Parsers::FilesTest: remove.

--- lib/eac_rails_utils.rb
+++ lib/eac_rails_utils.rb
@@ -36,7 +36,6 @@ module EacRailsUtils
   require 'eac/menus_helper/gui_builder'
   require 'eac/model'
   require 'eac/no_presence_validator'
-  require 'eac/parsers/files_test'
   require 'eac/source_target_fixtures'
   require 'eac/test_utils'
 
