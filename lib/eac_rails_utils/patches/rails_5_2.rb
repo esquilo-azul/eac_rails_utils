e25c5b24afc243d367fa85edf3a59b8bb747f641
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-04 17:52:29 -0300

    EacRailsUtils::Patches::Rails52.

---
+++ lib/eac_rails_utils/patches/rails_5_2.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'active_model/associations/hooks'
+require 'eac_ruby_utils/require_sub'
+
+module EacRailsUtils
+  module Patches
+    module Rails52
+      ::EacRubyUtils.require_sub __FILE__
+
+      class << self
+        def enabled?
+          ::Rails.version >= '5.2'
+        end
+      end
+    end
+  end
+end
