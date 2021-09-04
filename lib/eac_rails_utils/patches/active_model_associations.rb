4c2b34a3875888571348acc33469743b843cdfb2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-04 17:52:29 -0300

    EacRailsUtils::Patches::Rails52::ActiveModelAssociationMethodFix.

--- lib/eac_rails_utils/patches/active_model_associations.rb
+++ lib/eac_rails_utils/patches/active_model_associations.rb
@@ -2,6 +2,7 @@
 
 require 'activemodel/associations'
 require 'eac_rails_utils/patches/rails_4'
+require 'eac_rails_utils/patches/rails_5_2'
 
 module ActiveModel
   module Associations
@@ -9,6 +10,7 @@ module ActiveModel
       class << self
         def init
           init_rails_4 if ::EacRailsUtils::Patches::Rails4.enabled?
+          init_rails_5_2 if ::EacRailsUtils::Patches::Rails52.enabled?
         end
 
         def init_rails_4
@@ -16,6 +18,16 @@ module ActiveModel
             ActiveRecord::Associations::AssociationScope.prepend(
               ::EacRailsUtils::Patches::Rails4.ActiveRecordAssociationsAssociationScope
             )
+          end
+        end
+
+        def init_rails_5_2
+          rails_5_2_fix_activemodel_associations_methods
+        end
+
+        def rails_5_2_fix_activemodel_associations_methods
+          %i[belongs_to has_many].each do |method|
+            ::EacRailsUtils::Patches::Rails52::ActiveModelAssociationMethodFix.new(method)
           end
         end
       end
---
+++ lib/eac_rails_utils/patches/rails_5_2/active_model_association_method_fix.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'activemodel/associations'
+require 'eac_ruby_utils/core_ext'
+
+module EacRailsUtils
+  module Patches
+    module Rails52
+      class ActiveModelAssociationMethodFix
+        common_constructor :method_name do
+          perform
+        end
+
+        def original_method_new_name
+          "original_#{method_name}".to_sym
+        end
+
+        private
+
+        def the_module
+          ::ActiveModel::Associations::ClassMethods
+        end
+
+        def perform
+          patch = self
+          the_module.class_eval do
+            alias_method patch.original_method_new_name, patch.method_name
+            remove_method patch.method_name
+
+            define_method patch.method_name do |name, scope = nil, **options, &extension|
+              send(patch.original_method_new_name, name, scope, options, &extension)
+            end
+          end
+        end
+      end
+    end
+  end
+end
