7e8dbe0d8bcb5b51a6fb08057daaa6d2978b3b7f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-04 18:25:46 -0300

    EacRailsUtils::Patches::Rails4::ActiverecordAssociationsAssociationScope: extract from "eac_rails_utils/patches/active_model_associations.rb".

--- lib/eac_rails_utils/patches/active_model_associations.rb
+++ lib/eac_rails_utils/patches/active_model_associations.rb
@@ -1,27 +1,7 @@
 # frozen_string_literal: true
 
-require 'active_model/associations/hooks'
 require 'activemodel/associations'
-
-module EacRailsUtils
-  module Patches
-    module ActiveModelAssociations
-      module ScopeExtensionPatch
-        def add_constraints(scope, owner, association_klass, *extra_args)
-          if extra_args.any?
-            refl = extra_args.first
-            if refl.options[:active_model]
-              target_ids = refl.options[:target_ids]
-              return scope.where(id: owner[target_ids])
-            end
-          end
-
-          super
-        end
-      end
-    end
-  end
-end
+require 'eac_rails_utils/patches/rails_4'
 
 module ActiveModel
   module Associations
@@ -34,7 +14,7 @@ module ActiveModel
         def init_rails_4
           ActiveSupport.on_load(:active_record) do
             ActiveRecord::Associations::AssociationScope.prepend(
-              ::EacRailsUtils::Patches::ActiveModelAssociations::ScopeExtensionPatch
+              ::EacRailsUtils::Patches::Rails4::ActiveModelAssociations::ScopeExtensionPatch
             )
           end
         end
