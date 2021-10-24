8fc2d3c129e963f72b2cea8f2350b88bf777bd9a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-24 13:37:05 -0300

    EacRailsUtils::Rspec::Setup::ModelsUtils.

--- lib/eac_rails_utils/rspec/setup.rb
+++ lib/eac_rails_utils/rspec/setup.rb
@@ -6,6 +6,10 @@ module EacRailsUtils
   module Rspec
     module Setup
       require_sub __FILE__
+
+      def self.extended(setup_obj)
+        setup_obj.rspec_config.extend(::EacRailsUtils::Rspec::Setup::ModelsUtils)
+      end
     end
   end
 end
---
+++ lib/eac_rails_utils/rspec/setup/models_utils.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacRailsUtils
+  module Rspec
+    module Setup
+      module ModelsUtils
+        def model_record_attribute_test(record_variable, attribute, valid, value)
+          context("when #{record_variable}.#{attribute} == #{value}") do
+            before do
+              send(record_variable).send("#{attribute}=", value)
+            end
+
+            it "#{record_variable} should be {valid ? '' : 'not '}valid" do
+              expect(send(record_variable).valid?).to send("be_#{valid ? 'truthy' : 'falsy'}"),
+                                                      send(record_variable).errors.messages
+            end
+          end
+        end
+
+        def model_record_values_attribute_test(record_variable, attribute, valid, values)
+          values.each do |value|
+            model_record_attribute_test(record_variable, attribute, valid, value)
+          end
+        end
+
+        def model_record_valid_invalid_values_attribute_test(record_variable, attribute,
+                                                             valid_values, invalid_values)
+          {
+            false => invalid_values,
+            true => valid_values
+          }.each do |valid, values|
+            model_record_values_attribute_test(record_variable, attribute, valid, values)
+          end
+        end
+      end
+    end
+  end
+end
