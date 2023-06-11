045746e90c05276d9e552c17c5d6ca8548aa5ccd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-11 12:10:12 -0300

    EacRailsUtils::DataTableHelper::ColumnNode.initialize: refact with .common_constructor.

--- app/helpers/eac_rails_utils/data_table_helper/column_node.rb
+++ app/helpers/eac_rails_utils/data_table_helper/column_node.rb
@@ -5,20 +5,17 @@ require 'eac_ruby_utils/core_ext'
 module EacRailsUtils
   module DataTableHelper
     class ColumnNode
-      def initialize(node, path)
-        @node = node
-        @path = path
-      end
+      common_constructor :node, :path
 
       # @return [Object]
-      def value
-        return @node if @node.nil? || @path.empty?
+      def value # rubocop:disable Metrics/AbcSize
+        return node if node.nil? || path.empty?
 
-        subpath = @path.dup
+        subpath = path.dup
         n = subpath.shift
-        return self.class.new(@node.send(n), subpath).value if @node.respond_to?(n)
+        return self.class.new(node.send(n), subpath).value if node.respond_to?(n)
 
-        raise "Instance of #{@node.class} does not respond to #{n}"
+        raise "Instance of #{node.class} does not respond to #{n}"
       end
     end
   end
