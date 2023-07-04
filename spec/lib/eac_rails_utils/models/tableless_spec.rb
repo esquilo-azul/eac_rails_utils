14dec74bcf3a380b12f6c90423bd73cfe0f4b5a2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-07-04 00:46:15 -0300

    spec/lib/eac_rails_utils/models/tableless/belongs_to_spec: extrai de RSpec.describe(EacRailsUtils::Models::Tableless).

--- spec/lib/eac_rails_utils/models/tableless_spec.rb
+++ spec/lib/eac_rails_utils/models/tableless_spec.rb
@@ -10,8 +10,6 @@ RSpec.describe(::EacRailsUtils::Models::Tableless) do
       attribute :tempo, DateTime
 
       attribute :job_id, Integer
-      belongs_to :job, class_name: 'Job'
-      validates :job, presence: true
     end
   end
   let(:record) do
@@ -29,38 +27,5 @@ RSpec.describe(::EacRailsUtils::Models::Tableless) do
 
   describe '#columns_names' do
     it { expect(model.columns_names).to eq(%i[tempo job_id]) }
-  end
-
-  describe '#belongs_to' do
-    context 'when value is nil' do
-      before { record.job = nil }
-
-      it { expect(record).not_to be_valid }
-    end
-
-    context 'when value is set by foreign key' do
-      let(:job) { ::Job.create!(name: 'A Job') }
-
-      before do
-        record.job_id = job.id
-      end
-
-      it { expect(job.id).to be_present }
-      it { expect(record.job).to be_a(::Job) }
-      it { expect(record).to be_valid }
-    end
-
-    context 'when values has association type' do
-      before { record.job = ::Job.new }
-
-      it { expect(record.job).to be_a(::Job) }
-      it { expect(record).to be_valid }
-    end
-
-    context 'when value has not association type' do
-      it do
-        expect { record.job = ::User.new }.to raise_error(::ActiveRecord::AssociationTypeMismatch)
-      end
-    end
   end
 end
