3be5884a94d30fc061efbe151e366c1d1beeb76d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-09-13 08:50:43 -0300

    EacRailsUtils::Menus::Group.

---
+++ lib/eac_rails_utils/menus/group.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'eac_rails_utils/menus/action'
+require 'eac_rails_utils/menus/node'
+require 'eac_ruby_utils/core_ext'
+
+module EacRailsUtils
+  module Menus
+    class Group < ::EacRailsUtils::Menus::Node
+      class << self
+        # @param key [Object]
+        # @return [Symbol]
+        def sanitize_key(key)
+          key.to_sym
+        end
+      end
+
+      DEFAULT_PATH_FIRST_NODE = :main_app
+      SELF_LABEL_TRANSLATE_KEY_PART = '__self'
+
+      common_constructor :key, :parent_group, :path_first_node, default: [nil],
+                                                                super_args: -> { [parent_group] } do
+        self.key = self.class.sanitize_key(key)
+        self.path_first_node ||= DEFAULT_PATH_FIRST_NODE
+      end
+
+      # @param path [Array<Symbol>]
+      # @return [EacRailsUtils::Menus::Action]
+      def action(*path)
+        child_action = ::EacRailsUtils::Menus::Action.new(path, self)
+        actions[child_action.key] ||= child_action
+      end
+
+      # @return [Array<EacRailsUtils::Menus::Node>]
+      def children
+        (groups.values + actions.values).sort
+      end
+
+      # @return [EacRailsUtils::Menus::Group]
+      def group(group_key, path_first_node = nil)
+        child_group = self.class.new(group_key, self, path_first_node)
+        groups[child_group.key] ||= child_group
+      end
+
+      # @return [String]
+      def label_translate_key
+        [super, SELF_LABEL_TRANSLATE_KEY_PART].join(TRANSLATE_KEY_SEPARATOR)
+      end
+
+      # @param view [ActionView::Base]
+      # @return [Hash]
+      def to_dropdown_menu_entries(view)
+        children.inject({}) do |a, e|
+          a.merge(e.label => e.to_dropdown_menu_entries(view))
+        end
+      end
+
+      def within
+        yield(self)
+      end
+
+      private
+
+      def actions
+        @actions ||= {}
+      end
+
+      def groups
+        @groups ||= {}
+      end
+    end
+  end
+end
---
+++ spec/lib/eac_rails_utils/menus/group_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe(::EacRailsUtils::Menus::Group) do
+  let(:root) { described_class.new(:root, nil) }
+  let(:sub1) { root.group(:sub1) }
+  let(:sub1_1) { sub1.group(:sub1_1) }
+  let(:action1) { sub1.action(:action1) }
+  let(:action2) { sub1_1.action(:engine2, :action1) }
+
+  describe '#label_translate_key' do
+    {
+      root: 'eac_rails_utils.menus.root.__self',
+      sub1: 'eac_rails_utils.menus.root.sub1.__self',
+      sub1_1: 'eac_rails_utils.menus.root.sub1.sub1_1.__self',
+      action1: 'eac_rails_utils.menus.root.sub1.main_app_action1',
+      action2: 'eac_rails_utils.menus.root.sub1.sub1_1.engine2_action1'
+    }.each do |node_key, expected|
+      context "when node is \"#{node_key}\"" do
+        let(:node) { send(node_key) }
+
+        it do
+          expect(node.label_translate_key).to eq(expected)
+        end
+      end
+    end
+  end
+end
