9dad74a7093a4a4a0b32379d37788aa515bc7361
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-09-18 15:40:15 -0300

    .rubocop.yml#Style/FrozenStringLiteralComment: remove exclusions.

--- lib/eac_rails_utils/models/tableless_associations.rb
+++ lib/eac_rails_utils/models/tableless_associations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'eac_rails_utils/models/tableless_associations/initialize_extension'
 require 'eac_rails_utils/models/tableless_associations/active_record_reflection'
 require 'eac_rails_utils/models/tableless_associations/autosave_association'
--- lib/eac_rails_utils/models/tableless_associations/association_scope_extension.rb
+++ lib/eac_rails_utils/models/tableless_associations/association_scope_extension.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module EacRailsUtils
   module Models
     module TablelessAssociations
