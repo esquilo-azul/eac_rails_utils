69ad793187beb4e37b5446d686c8640a7392f164
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-06 14:37:42 -0300

    RSpec.describe('EacRailsUtils::Models::Tableless#has_many').

---
+++ spec/lib/eac_rails_utils/models/tableless/has_many_to_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+::RSpec.describe(::EacRailsUtils::Models::Tableless) do
+  let(:tableless_job_model) do
+    ::Class.new(described_class) do
+      def self.name
+        'TablelessJob'
+      end
+
+      attribute :id, Integer
+      has_many :users, class_name: 'User', foreign_key: :job_id
+
+      def self.primary_key
+        'id'
+      end
+    end
+  end
+  let(:real_job) { ::Job.create! }
+  let(:user1) { ::User.create!(job: real_job, email: 'user1@example.com', password: 'abc123') }
+  let(:user2) { ::User.create!(job: real_job, email: 'user2@example.com', password: 'abc123') }
+  let(:tableless_job_record) { tableless_job_model.new }
+
+  describe '#has_many' do
+    before do
+      user1
+      user2
+      tableless_job_record.id = real_job.id
+    end
+
+    it { expect(real_job.users.count).to eq(2) }
+    it { expect(tableless_job_record.id).to be_present }
+    it { expect(tableless_job_record.users.count).to eq(2) }
+  end
+end
