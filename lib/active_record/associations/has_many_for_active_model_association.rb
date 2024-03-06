d0c0ee6a71e37f83e4a64d177dc3b5b1bbe502fe
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-06 13:29:05 -0300

    .rubocop.yml#Metrics/MethodLength: remove.

--- lib/active_record/associations/has_many_for_active_model_association.rb
+++ lib/active_record/associations/has_many_for_active_model_association.rb
@@ -20,7 +20,7 @@ module ActiveRecord::Associations
     end
 
     # full replace simplely
-    def replace(other_array) # rubocop:disable Metrics/AbcSize
+    def replace(other_array) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       original_target = load_target.dup
       other_array.each { |val| raise_on_type_mismatch!(val) }
       target_ids = reflection.options[:target_ids]
@@ -41,7 +41,7 @@ module ActiveRecord::Associations
     end
 
     # no need transaction
-    def concat(*records) # rubocop:disable Metrics/AbcSize
+    def concat(*records) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       load_target
       flatten_records = records.flatten
       flatten_records.each { |val| raise_on_type_mismatch!(val) }
