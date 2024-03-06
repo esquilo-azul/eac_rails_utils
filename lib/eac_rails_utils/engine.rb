249ccca29994942541ed7d282db554ca3d6d889d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-06 14:28:38 -0300

    EacRailsUtils::Engine: include EacRailsUtils::EngineHelper.

--- lib/eac_rails_utils/engine.rb
+++ lib/eac_rails_utils/engine.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require 'eac_rails_utils/engine_helper'
 require 'nested_form_fields'
 
 module EacRailsUtils
-  class Engine < ::Rails::Engine; end
+  class Engine < ::Rails::Engine
+    include ::EacRailsUtils::EngineHelper
+  end
 end
