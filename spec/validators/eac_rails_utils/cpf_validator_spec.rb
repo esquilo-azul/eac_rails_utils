d00195d6e793eaa9bb1f75c03ea79072fcc7df3d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-17 16:53:02 -0300

    RSpec.describe(EacRailsUtils::CpfValidator): refact with RSpec.shared_examples('active_model_attribute_validator').

--- spec/validators/eac_rails_utils/cpf_validator_spec.rb
+++ spec/validators/eac_rails_utils/cpf_validator_spec.rb
@@ -1,36 +1,16 @@
 # frozen_string_literal: true
 
 ::RSpec.describe(::EacRailsUtils::CpfValidator) do
-  let(:model) do
-    ::Class.new do
-      include ActiveModel::Model
-
-      attr_accessor :cpf
-
-      validates :cpf, 'eac_rails_utils/cpf' => true, allow_nil: true
-    end
-  end
-
-  let(:record) { model.new }
-
-  {
-    '85630275305' => true, '66244374487' => true, nil => true,
-    '' => false, ' ' => false, 'abc' => false, '856.302.753-05' => false, '662.443.744-87' => false,
-    '85630275304' => false
-  }.each do |cpf, valid|
-    context "when cpf is \"#{cpf}\"" do
-      before do
-        record.cpf = cpf
-        record.valid?
-      end
-
-      if valid
-        it { expect(record).to be_valid }
-        it { expect(record.errors[:cpf]).to eq([]) }
-      else
-        it { expect(record).not_to be_valid }
-        it { expect(record.errors[:cpf]).not_to eq([]) }
-      end
-    end
-  end
+  include_examples 'active_model_attribute_validator', [
+    '85630275305',
+    '66244374487',
+    nil
+  ], [
+    '',
+    ' ',
+    'abc',
+    '856.302.753-05',
+    '662.443.744-87',
+    '85630275304'
+  ]
 end
