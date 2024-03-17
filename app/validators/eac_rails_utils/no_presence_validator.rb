7b75f037c2f10b483ca066d81b1285b92bf64420
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-17 20:12:51 -0300

    EacRailsUtils::NoPresenceValidator: fix deprecated.

--- app/validators/eac_rails_utils/no_presence_validator.rb
+++ app/validators/eac_rails_utils/no_presence_validator.rb
@@ -6,7 +6,7 @@ module EacRailsUtils
     def validate_each(record, attribute, _value)
       return if record.send(attribute).blank?
 
-      record.errors[attribute] << (options[:message] || 'must be blank') # rubocop:disable Rails/DeprecatedActiveModelErrorsMethods
+      record.errors.add(attribute, options[:message] || 'must be blank')
     end
   end
 end
