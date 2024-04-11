7fed7c94a37031353dc1b6d4ed1e7ac462959ea5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-04-10 22:54:41 -0300

    .rubocop.yml#Style/MutableConstant: remove.

--- lib/active_record/associations/builder/has_many_for_active_model.rb
+++ lib/active_record/associations/builder/has_many_for_active_model.rb
@@ -2,7 +2,7 @@ module ActiveRecord::Associations::Builder # rubocop:disable Style/ClassAndModul
   class HasManyForActiveModel < HasMany
     if ActiveRecord.version >= Gem::Version.new("5.0.0.beta")
       AR_CALLBACK_METHODS = %i(define_callback before_validation after_validation before_save
-                               after_save before_update after_update)
+                               after_save before_update after_update).freeze
 
       def self.valid_options(_options)
         super + [:active_model,
