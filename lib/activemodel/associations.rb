3c2293e18823c27f3e699cdc22fb2bce532d2b2d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-04-10 23:05:54 -0300

    .rubocop.yml#Style/StringLiterals: remove.

--- lib/activemodel/associations.rb
+++ lib/activemodel/associations.rb
@@ -1,13 +1,13 @@
-require "active_model" # rubocop:disable Style/FrozenStringLiteralComment
-require "active_record"
-require "active_support"
-require "eac_rails_utils/models/tableless_associations"
-require "eac_rails_utils/models/tableless_associations/hooks"
+require 'active_model' # rubocop:disable Style/FrozenStringLiteralComment
+require 'active_record'
+require 'active_support'
+require 'eac_rails_utils/models/tableless_associations'
+require 'eac_rails_utils/models/tableless_associations/hooks'
 
 # Load Railtie
 begin
-  require "rails"
+  require 'rails'
 rescue LoadError # rubocop:disable Lint/SuppressedException
 end
 
-require "eac_rails_utils/models/tableless_associations/railtie" if defined?(Rails)
+require 'eac_rails_utils/models/tableless_associations/railtie' if defined?(Rails)
