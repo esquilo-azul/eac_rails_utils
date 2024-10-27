4e7c4dafca1c092ec0c1a0f23ab5e6513135ab39
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-10-27 15:12:10 -0300

    EacRailsUtils::LinksHelper.detail_link.

--- app/helpers/eac_rails_utils/links_helper.rb
+++ app/helpers/eac_rails_utils/links_helper.rb
@@ -4,6 +4,7 @@ module EacRailsUtils
   module LinksHelper
     LINK_OPTIONS = { target: '_blank' }.freeze
     SHORT_LINK_OPTIONS = LINK_OPTIONS.merge(name: '').freeze
+    DETAIL_LINK_OPTIONS = LINK_OPTIONS.merge(action: 'detail')
     SHORT_DELETE_LINK_OPTIONS = SHORT_LINK_OPTIONS.merge(
       action: '', class: 'delete_link', method: :delete,
       title_translation: 'eac_rails_utils.links.delete_object',
@@ -17,6 +18,13 @@ module EacRailsUtils
     ).freeze
     SHORT_DETAIL_LINK_OPTIONS = SHORT_DETAIL_SHOW_LINK_OPTIONS.merge(action: 'detail')
     SHORT_SHOW_LINK_OPTIONS = SHORT_DETAIL_SHOW_LINK_OPTIONS.merge(action: '')
+
+    # @param object [Object]
+    # @param options [Hash]
+    # @return [ActiveSupport::SafeBuffer]
+    def detail_link(object, **options)
+      object_link object, DETAIL_LINK_OPTIONS.merge(options)
+    end
 
     def short_delete_link(object)
       object_link object, SHORT_DELETE_LINK_OPTIONS
