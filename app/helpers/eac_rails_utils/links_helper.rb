23d1316a147130c4dbc0c8890530056b6cd91c04
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-10-27 15:02:29 -0300

    EacRailsUtils::LinksHelper::SHORT_LINK_OPTIONS: convert from .short_object_link.

--- app/helpers/eac_rails_utils/links_helper.rb
+++ app/helpers/eac_rails_utils/links_helper.rb
@@ -2,28 +2,27 @@
 
 module EacRailsUtils
   module LinksHelper
-    SHORT_DELETE_LINK_OPTIONS = {
-      action: '', class: 'delete_link', method: :delete, target: '_blank',
+    SHORT_LINK_OPTIONS = { name: '', target: '_blank' }.freeze
+    SHORT_DELETE_LINK_OPTIONS = SHORT_LINK_OPTIONS.merge(
+      action: '', class: 'delete_link', method: :delete,
       title_translation: 'eac_rails_utils.links.delete_object',
       confirm_translation: 'eac_rails_utils.links.delete_confirm'
-    }.freeze
-    SHORT_EDIT_LINK_OPTIONS = {
-      action: 'edit', class: 'edit_link', target: '_blank',
-      title_translation: 'eac_rails_utils.links.edit_object'
-    }.freeze
-    SHORT_DETAIL_SHOW_LINK_OPTIONS = {
-      class: 'show_link', target: '_blank',
-      title_translation: 'eac_rails_utils.links.show_object'
-    }.freeze
+    ).freeze
+    SHORT_EDIT_LINK_OPTIONS = SHORT_LINK_OPTIONS.merge(
+      action: 'edit', class: 'edit_link', title_translation: 'eac_rails_utils.links.edit_object'
+    ).freeze
+    SHORT_DETAIL_SHOW_LINK_OPTIONS = SHORT_LINK_OPTIONS.merge(
+      class: 'show_link', title_translation: 'eac_rails_utils.links.show_object'
+    ).freeze
     SHORT_DETAIL_LINK_OPTIONS = SHORT_DETAIL_SHOW_LINK_OPTIONS.merge(action: 'detail')
     SHORT_SHOW_LINK_OPTIONS = SHORT_DETAIL_SHOW_LINK_OPTIONS.merge(action: '')
 
     def short_delete_link(object)
-      short_object_link object, SHORT_DELETE_LINK_OPTIONS
+      object_link object, SHORT_DELETE_LINK_OPTIONS
     end
 
     def short_edit_link(object)
-      short_object_link object, SHORT_EDIT_LINK_OPTIONS
+      object_link object, SHORT_EDIT_LINK_OPTIONS
     end
 
     def short_goto_link(url)
@@ -35,17 +34,11 @@ module EacRailsUtils
     end
 
     def short_show_link(object)
-      short_object_link object, SHORT_SHOW_LINK_OPTIONS
+      object_link object, SHORT_SHOW_LINK_OPTIONS
     end
 
     def short_detail_link(object)
-      short_object_link object, SHORT_DETAIL_LINK_OPTIONS
-    end
-
-    private
-
-    def short_object_link(object, options = {})
-      object_link(object, options.merge(name: ''))
+      object_link object, SHORT_DETAIL_LINK_OPTIONS
     end
 
     require_sub __FILE__, require_mode: :kernel
