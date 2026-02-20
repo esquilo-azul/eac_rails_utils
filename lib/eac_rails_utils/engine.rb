5122171962a44c19b70b4255b6689e62d17c6079
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-20 14:06:47 -0300

    EacRailsUtils: setup with Rails::Engine.

--- lib/eac_rails_utils/engine.rb
+++ lib/eac_rails_utils/engine.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+require 'rails'
+require 'action_view' # Fix "require 'nested_form_fields'"
+require 'active_record'
+require 'nested_form_fields'
+require 'eac_ruby_utils'
+require 'virtus'
+
+require 'eac_rails_utils/engine_helper'
+
 module EacRailsUtils
   class Engine < ::Rails::Engine
     include ::EacRailsUtils::EngineHelper
   end
 end
+
+require 'eac_rails_utils/patches'
