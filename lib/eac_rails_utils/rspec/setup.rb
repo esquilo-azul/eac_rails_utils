e39fceea84896608e1fc3c10ffbe8c806bc0612a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-21 13:13:42 -0300

    EacRailsUtils::Rspec::Setup: fix missing require.

--- lib/eac_rails_utils/rspec/setup.rb
+++ lib/eac_rails_utils/rspec/setup.rb
@@ -4,6 +4,7 @@ module EacRailsUtils
   module Rspec
     module Setup
       def self.extended(setup_obj)
+        require 'eac_rails_utils/rspec/setup/models_utils'
         setup_obj.rspec_config.extend(::EacRailsUtils::Rspec::Setup::ModelsUtils)
       end
     end
